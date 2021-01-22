1634f55364fe0748e7dee4bf6976afbb3fe85c46
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-23 02:24:23 +0900

    JavaScriptテスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.alert('クリックされた')
+})
