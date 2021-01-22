77c9d203ef5b50142d97a8bb509d3c8121c6cb1c
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-23 01:06:34 +0900

    editページを作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,25 @@ class ProfilesController < ApplicationController
     @profile = current_user.profile
   end
   
+  def edit
+    @profile = current_user.profile
+  end
+  
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを保存しました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+  private
+  
+  def profile_params
+    params.permit(:gender, :birthday, :subscribed)
+  end
+  
   
 
 end
