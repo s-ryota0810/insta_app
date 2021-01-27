0708b1249e7c6ce78343009eb32d0061267337e4
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-27 23:35:33 +0900

    Likeテーブルの作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many_attached :images, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
 
 
---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :article
+  validates :user, presence: true
+  validates :article, presence: true
+  
+end
