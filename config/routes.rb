fed57889dd640aac82822b51a2ddcf1ace567ded
ryota.s.0810 <akaashi0810@gmail.com>
2021-02-01 21:39:11 +0900

    アカウントページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :accounts, only: [:show, :edit, :update]
   root to: 'articles#index'
   resource :profile
   resources :articles do
