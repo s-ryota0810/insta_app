ca399eaa6358c7cb0d388972e3540b841b7203ac
ryota.s.0810 <akaashi0810@gmail.com>
2021-02-01 14:22:59 +0900

    コメントの作成機能の追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
+  def index
+    @article = Article.find(params[:article_id])
+    @comments = @article.comments
+  end
+  
+  def new
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build
+  end
+  
+  def create
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(comments_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to article_comments_path(article)
+    else
+      render :new
+    end
+  end  
+  
+  
+  private
+  
+  def comments_params
+    params.require(:comment).permit(:content, :user_id )
+  end
+end
