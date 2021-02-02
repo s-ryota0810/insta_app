163bba369c39032f3074d761e4cd5290c7fb5271
ryota.s.0810 <akaashi0810@gmail.com>
2021-02-02 22:06:57 +0900

    following一覧ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :accounts, only: [:show, :edit, :update] do
     resource :follows, only: [:create, :destroy, :show]
+    resources :followings, only: [:index, :show]
   end
   root to: 'articles#index'
   resource :profile
