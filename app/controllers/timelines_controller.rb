f70bee10f5eb7099349323dab5c48dcd1ffa4fd2
ryota.s.0810 <akaashi0810@gmail.com>
2021-02-04 23:33:20 +0900

    記事一覧を降順（新着順）に並び替え

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -3,6 +3,11 @@ class TimelinesController < ApplicationController
   
   def show
     followings_ids = current_user.followings.pluck(:id)
-    @articles = Article.where(user_id: followings_ids) #配列を渡すことで、or検索に
+    @articles = Article.where(user_id: followings_ids).order(created_at: 'DESC') #配列を渡すことで、or検索に
+  end
+  
+  
+  def add_me
+    
   end
 end
