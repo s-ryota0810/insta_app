0716331809c7e3ab165e62c059e4b2da0de230ec
ryota.s.0810 <akaashi0810@gmail.com>
2021-02-04 23:04:02 +0900

    タイムライン機能を追加して、ルートに設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :timeline, only: [:show]
   resources :accounts, only: [:show, :edit, :update] do
     resource :follows, only: [:create, :destroy, :show]
     resources :followings, only: [:index]
     resources :followers, only: [:index]
   end
-  root to: 'articles#index'
+  root to: 'timelines#show'
   resource :profile
   resources :articles do
     resources :images
