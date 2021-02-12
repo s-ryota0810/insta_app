0973e1963661fa24a0739cb796f2cd8ba14d7f1d
ryota.s.0810 <akaashi0810@gmail.com>
2021-02-12 22:17:10 +0900

    リファクタリング

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,40 +37,4 @@ document.addEventListener('DOMContentLoaded', () => {
   
 })
 
-document.addEventListener('turbolinks:load', () => {
-  
-  const accountDataset = $('#account-show').data()
-  const accountId = accountDataset.accountId
-
-  axios.get(`/accounts/${accountId}/follows`)
-      .then((res) => {
-        const followed = res.data.followStatus
-        if (followed) {
-          $('.unfollow-btn').removeClass('hidden')
-        } else {
-          $('.follow-btn').removeClass('hidden')
-        }
-     })
-     
-  $('.unfollow-btn').on('click', () => {
-    axios.delete(`/accounts/${accountId}/follows`)
-      .then((res) => {
-        if (res.data.status === 'ok') {
-          $('.follow-btn').removeClass('hidden')
-          $('.unfollow-btn').addClass('hidden')
-        }
-      })
-  })
-  
-  $('.follow-btn').on('click', () => {
-    axios.post(`/accounts/${accountId}/follows`)
-      .then((res) => {
-        if (res.data.status === 'ok') {
-          $('.unfollow-btn').removeClass('hidden')
-          $('.follow-btn').addClass('hidden')
-        }
-      })
-  })
-
-})
 
